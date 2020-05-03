22c3fb62d855bc4f6bc61c0e04bca30b2aa9bc82
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2020-05-04 02:07:58 +0300

    completed

--- attributes.py
+++ attributes.py
@@ -1,6 +1,9 @@
 from abc import abstractmethod
 from collections import Counter
-
+from math import floor
+from math import ceil
+from math import sqrt
+from math import pow
 import numpy
 
 
@@ -16,6 +19,20 @@ class Attribute:
     def num_of_values(self) -> int:
         return len(list(filter(lambda x: x is not None, self.values)))
 
+    def percent_of_missing_values(self) -> float:
+        empty_values = 0
+
+        for value in self.values:
+            if value:
+                continue
+
+            empty_values += 1
+
+        return empty_values / len(self.values) * 100
+
+    def cardinality(self) -> int:
+        return len(Counter(self.values).keys())
+
     @abstractmethod
     def fill_missing_values(self) -> None:
         pass
@@ -29,10 +46,32 @@ class NumericAttribute(Attribute):
     def max_value(self) -> float:
         return max(filter(lambda x: x is not None, self.values))
 
+    def quartile(self, index: int) -> float:
+        if index not in [1, 2, 3]:
+            raise ValueError("Invalid quartile index, possible indexes: 1, 2, 3")
+
+        values = list(filter(lambda x: x is not None, self.values))
+
+        values.sort()
+
+        index = len(values) * 0.25 * index
+
+        if index % 1 != 0:
+            return (values[floor(index)] + values[ceil(index)]) / 2
+        else:
+            return values[int(index)]
+
     def average(self) -> float:
         values = list(filter(lambda x: x is not None, self.values))
 
         return sum(values) / len(values)
+
+    def standard_deviation(self) -> float:
+        values = list(filter(lambda x: x is not None, self.values))
+
+        values = list(map(lambda x: x * x, values))
+
+        return sqrt(sum(values) / len(values) - pow(self.average(), 2))
 
     def fill_missing_values(self) -> None:
         avg = self.average()
@@ -49,6 +88,16 @@ class NumericAttribute(Attribute):
         for index, value in enumerate(self.values):
             if abs(value - mean_value) > m * deviation:
                 self.values[index] = average
+
+    def get_normalized_values(self) -> list:
+        new_values = []
+        minimum = self.min_value()
+        maximum = self.max_value()
+
+        for value in self.values:
+            new_values.append(((value - minimum) / (maximum - minimum)) * (1))
+
+        return new_values
 
 
 class CategoricalAttribute(Attribute):
@@ -77,3 +126,30 @@ class CategoricalAttribute(Attribute):
         for index, value in enumerate(self.values):
             if not value:
                 self.values[index] = mode
+
+
+def print_numeric_attribute_info(attributes: [Attribute]) -> None:
+    print('{:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20}'.format("Pavadinimas", "kiekis",
+                                                                                                "missing value %",
+                                                                                                "kardinalumas", "min",
+                                                                                                "max", "1 quart",
+                                                                                                "3 quart", "average",
+                                                                                                "mediana", "deviation"))
+
+    for attribute in attributes:
+        if not isinstance(attribute, NumericAttribute):
+            continue
+
+        print('{:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20} {:>20}'.format(
+            attribute.name,
+            attribute.num_of_values(),
+            attribute.percent_of_missing_values(),
+            attribute.cardinality(),
+            attribute.min_value(),
+            attribute.max_value(),
+            attribute.quartile(1),
+            attribute.quartile(3),
+            attribute.average(),
+            attribute.quartile(2),
+            attribute.standard_deviation()
+        ))
--- stock_predictions.py
+++ stock_predictions.py
@@ -1,6 +1,7 @@
 from file_reader import read_file
 import neurolab as nl
-import pylab as pl
+import numpy as np
+from attributes import print_numeric_attribute_info
 
 # read stock data
 attribute_types = {
@@ -15,78 +16,101 @@ attribute_types = {
 
 data = read_file("stock.csv", attribute_types)
 
-# input count for a single output
-n = 6
+print_numeric_attribute_info(data)
 
 # learning rate
 learning_rate = 0.000005
 
 # maximum number of epochs
-epochs = 10000
+epochs = 500
 
 # after how many epochs to display error
-epochs_to_show = 20
-
-# get sunspot activity value range
-min_value = data[1].min_value()
-max_value = data[1].max_value()
+epochs_to_show = 100
 
 # get input ranges
-input_ranges = []
+input_ranges = [
+    [data[0].min_value(), data[0].max_value()],
+    [data[1].min_value(), data[1].max_value()],
+    [data[2].min_value(), data[2].max_value()],
+    [data[3].min_value(), data[3].max_value()],
+    [data[4].min_value(), data[4].max_value()],
+    [data[5].min_value(), data[5].max_value()]
+]
 
-for i in range(n):
-    input_ranges.append([min_value, max_value])
 
-# create a perceptron with n inputs
-net = nl.net.newp(input_ranges, 1)
+def perform_simulation(range_start: int, range_end: int) -> float:
+    # create a perceptron with 6 inputs
+    net = nl.net.newp(input_ranges, 1)
 
-# set transfer function to linear
-net.layers[0].transf = nl.trans.PureLin()
+    # set transfer function to linear
+    net.layers[0].transf = nl.trans.PureLin()
 
-# set error function to MSE
-net.errorf = nl.error.MSE()
+    # set error function to MSE
+    net.errorf = nl.error.MSE()
 
-# set training error goal
-net.trainf.defaults["goal"] = 100
+    # set training error goal
+    net.trainf.defaults["goal"] = 1
 
-# get training input and output
-training_input = []
-training_output = []
+    # get training input and output
+    training_input = []
+    training_output = []
 
-chunk_size = n + 1
+    for i in range(range_start, int(range_end - ((range_end - range_start) * 0.25))):
+        training_input.append([
+            data[0].values[i],
+            data[1].values[i],
+            data[2].values[i],
+            data[3].values[i],
+            data[4].values[i],
+            data[5].values[i]
+        ])
 
-for training_data in [data[1].values[i:i + chunk_size] for i in range(0, 200, chunk_size)]:
-    training_input_set = []
+        training_output.append([data[6].values[i]])
 
-    for index in range(len(training_data) - 1):
-        training_input_set.append(training_data[index])
+    # train the perceptron
+    net.train(training_input, training_output, epochs=epochs, lr=learning_rate, show=epochs_to_show)
 
-    training_input.append(training_input_set)
-    training_output.append([training_data[len(training_data) - 1]])
-
-# train the perceptron and get training error matrix6
-error = net.train(training_input, training_output, epochs=epochs, lr=learning_rate, show=epochs_to_show)
-
-# plot error
-pl.plot(error)
-pl.xlabel('Epoch number')
-pl.ylabel('Train error')
-pl.grid()
-pl.show()
-
-# make predictions with trained net for the remaining data
-print("Testing perceptron predictions...")
-
-for prediction_data in [data[1].values[200:][i:i + chunk_size] for i in range(0, len(data[1].values[200:]), chunk_size)]:
+    # make predictions with trained net for the remaining data
     prediction_inputs = []
+    expected_predictions = []
 
-    for index in range(len(prediction_data) - 1):
-        prediction_inputs.append(prediction_data[index])
+    for i in range(int(range_end - ((range_end - range_start) * 0.25)), range_end):
+        prediction_inputs.append([
+            data[0].values[i],
+            data[1].values[i],
+            data[2].values[i],
+            data[3].values[i],
+            data[4].values[i],
+            data[5].values[i]
+        ])
 
-    if len(prediction_inputs) != n:
-        break
+        expected_predictions.append(data[6].values[i])
 
-    expected_result = prediction_data[len(prediction_data) - 1]
-    actual_result = net.sim([prediction_inputs])
+    actual_predictions = [i[0] for i in net.sim(prediction_inputs)]
 
-    print('Expected prediction: {:>5}  Actual prediction: {:>5}'.format(expected_result, round(actual_result[0][0], 1)))
+    error = []
+
+    for i in range(0, len(expected_predictions)):
+        error.append(abs(expected_predictions[i] - actual_predictions[i]))
+
+    return np.average(error)
+
+
+errors = [
+    perform_simulation(0, 100),
+    perform_simulation(100, 200),
+    perform_simulation(200, 300),
+    perform_simulation(300, 400),
+    perform_simulation(400, 500),
+    perform_simulation(500, 600),
+    perform_simulation(600, 700),
+    perform_simulation(700, 800),
+    perform_simulation(800, 900),
+    perform_simulation(900, 1000),
+]
+
+# 1.8891 - 0.002
+# 1.7555 - 0.000005
+# ~7%
+
+print(f"Average error: {np.average(errors)}")
